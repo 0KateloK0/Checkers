9c5e18026cb9da25aee569380b60a1b5422528a9
0KateloK0 <katelkin2003@mail.ru>
2020-05-26 16:40:56 +0500

    First logic without separation and with huge bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Checkers from './checkers.js';
 
 function App() {
 	return (
-		<div></div>
+		<Checkers />
 	);
 }
 
